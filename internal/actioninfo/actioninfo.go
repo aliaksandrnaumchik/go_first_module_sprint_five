6612361f2234243b14a865cc5b4e9cf5d6f82a57
anaumcik <naumcik.inno.co@gmail.com>
2025-08-17 18:01:19 +0300

    update actioninfo

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,25 @@
 package actioninfo
 
+import (
+	"fmt"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(datastring string) (err error)
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	for _, data := range dataset {
+		if err := dp.Parse(data); err != nil {
+			fmt.Printf("Ошибка парсинга данных: %v", err)
+			continue
+		}
+
+		if info, err := dp.ActionInfo(); err != nil {
+			fmt.Printf("Ошибка получения информации")
+		} else {
+			fmt.Println(info)
+		}
+	}
 }
