7e75368e431e12fdabcd85604bc716d390f9b5de
anaumcik <naumcik.inno.co@gmail.com>
2025-08-19 12:48:53 +0300

    translate even more log events to english

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -12,12 +12,12 @@ type DataParser interface {
 func Info(dataset []string, dp DataParser) {
 	for _, data := range dataset {
 		if err := dp.Parse(data); err != nil {
-			fmt.Printf("Ошибка парсинга данных: %v", err)
+			fmt.Printf("Parsing data error: %v", err)
 			continue
 		}
 
 		if info, err := dp.ActionInfo(); err != nil {
-			fmt.Printf("Ошибка получения информации")
+			fmt.Printf("error during get action info from dataset")
 		} else {
 			fmt.Println(info)
 		}
